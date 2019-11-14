bcbdd7a6da67d287d8cdba251475c042567aebb6
smaheshwaram <smaheshwaram1@gmail.com>
2019-11-14 17:24:49 -0600

    Commit 3, Making query call from db module instead of app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-// module.exports = {
-//     getUsers,
-// }
+
 const express = require('express')
 const app = express()
 const cors = require('cors');
@@ -25,24 +23,6 @@ app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-
-
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    port: 5432,
-})
-// app.get('/users', db.getUsers)
-app.get('/users', (request, response) => {
-
-    pool.query('SELECT * FROM user_account ORDER BY id ASC', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-});
+app.get('/users', db.getUsers)
 
 module.exports = app;
--- db/queries.js
+++ db/queries.js
@@ -15,3 +15,7 @@ const getUsers = (request, response) => {
         response.status(200).json(results.rows)
     })
 };
+
+module.exports = {
+    getUsers,
+}
